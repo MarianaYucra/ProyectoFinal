8db3f3f0edd1d07aa97fadaec6bcd0955fe377d5
MarianaYucra <myucrama@unsa.edu.pe>
2020-07-14 23:12:42 -0500

    avance de listar

--- ControlVentas/src/ventas/views.py
+++ ControlVentas/src/ventas/views.py
@@ -4,3 +4,10 @@ from .models import Venta
 # Create your views here.
 def index (request):
     return render(request,"index.html")
+
+def listar(request):
+    obj = Venta.objects.all()
+    context = {
+        'objeto' : obj,
+    }
+    return render(request, "listar.html", context)
