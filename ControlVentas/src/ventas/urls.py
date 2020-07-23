75f3fe6d5577b51fa5e34449e6e79544c97a104c
MarianaYucra <myucrama@unsa.edu.pe>
2020-07-22 23:29:50 -0500

    agregando template para el delete de producto y categoria. Agregando listar, modificar y detallar para ventas

--- ControlVentas/src/ventas/urls.py
+++ ControlVentas/src/ventas/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from . import views
+from .views import (VentaListView, VentaDetailView, VentaUpdateView)
 
+
+app_name = 'ventas'
 urlpatterns = [
     path('', views.index, name="index"),
-    path('listar/', views.listar, name = "listar"),
+    #path('listar/', views.listar, name = "listar"),
+    path('ventas/',VentaListView.as_view(),name = 'venta-list'),
+    path('ventas/<int:pk>', VentaDetailView.as_view(), name = 'venta-detail'),
+    path('ventas/<int:pk>/update/',VentaUpdateView.as_view(),name = 'venta-update'),
 
 ]
