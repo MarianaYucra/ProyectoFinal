6a7170c6ccfd5ea2da458587da5f8ce987bd781f
Walter Huaracha <whuaracha@unsa.edu.pe>
2020-07-29 23:49:09 -0500

    Modificando los templates de mostrar detalles

--- ControlVentas/src/ControlVentas/urls.py
+++ ControlVentas/src/ControlVentas/urls.py
@@ -21,8 +21,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('ventas.urls')),
-    path('', include('clientes.urls')),
+    path('ventas/', include('ventas.urls')),
+    path('clientes/', include('clientes.urls')),
     path('productos/', include('productos.urls')),
     path('accounts/', include('accounts.urls')),
 ]
